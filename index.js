fb4188505f14c0645d398238f9db67e0870bf350
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-02 12:00:05 -0300

    Express configurado

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
