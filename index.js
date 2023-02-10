7f6af1763adb538b58aa2c73eb4d2107dfbfdddb
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-10 11:42:21 -0300

    URL de conexão atualizada

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // localhost ou 127.0.0.1
-const DB_URL = "mongodb://127.0.0.1:27017";
+// const DB_URL = "mongodb://127.0.0.1:27017";
+const DB_URL =
+  "mongodb+srv://admin:fsdLuHLFo4KwOZ7N@cluster0.bndngam.mongodb.net";
 const DB_NAME = "ocean-bancodados-09-02-2023";
 
 async function main() {
