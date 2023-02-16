18a1e637f7e2128db5019067577acee27445203f
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-16 12:04:02 -0300

    Criação dos exercícios no ReadAll

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,3 +1,18 @@
+/**
+ * Exercício 1:
+ * Dentro do componente ReadAll, crie um div com a classe "Card"
+ * Dentro do Card, exiba uma imagem de qualquer personagem
+ * Além disso, tb exiba o nome da parsonagem
+ * 
+ * OBS: caso vc tenha escolhido algo diferente no backend, pode manter o tema original
+ * Não precisa ser integrado com o Back! Apenas a exibição dos dados no Front.
+ * 
+ * 
+ * Exercício 2 (bônus!):
+ * Pegue o div com a classe "Card" e extraia em um componente JSX, seguindo os mesmos
+ * passos que fizemos para a criação do componente ReadAll
+ */
+
 function ReadAll() {
     return <div>Read All</div>
 }
