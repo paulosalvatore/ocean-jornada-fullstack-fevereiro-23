aa6bd596477099f164d92f7b7594ad200ff0739f
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-17 09:38:18 -0300

    Exercício corrigido

---
+++ frontend/src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+        <div className="card">
+            <h1>Rick Sanchez</h1>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
+    )
+}
+
+export default Card;
