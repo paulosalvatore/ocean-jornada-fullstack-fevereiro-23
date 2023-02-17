0c07f4ee6394ed39739957555f22dca424e96c8f
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-17 12:02:33 -0300

    Dados atualizados

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -6,14 +6,29 @@ import Card from "../Card/Card";
 // comportamento, sem precisar depender do back para receber dados
 const items = [
     {
-        _id: '1234',
-        nome: 'Rick Sanchez',
-        imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+        _id: "63ee1e0b18f2b9a93da8435a",
+        nome: "Rick Sanchez",
+        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
     },
     {
-        _id: '5678',
-        nome: 'Morty Smith',
-        imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+        _id: "63ef87c74522de2944ab1fa8",
+        nome: "Morty Smith",
+        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    },
+    {
+        _id: "63ef87cc4522de2944ab1fa9",
+        nome: "Summer Smith",
+        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    },
+    {
+        _id: "63ef87d44522de2944ab1faa",
+        nome: "Beth Smith",
+        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    },
+    {
+        _id: "63ef87e24522de2944ab1fab",
+        nome: "Jerry Smith",
+        imagemUrl: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
     },
 ];
 
