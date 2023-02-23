5294b4a2faf1a06c083c5c21ed1892eec0c440ed
Paulo Salvatore <salvatoregames@gmail.com>
2023-02-23 09:37:31 -0300

    Criação do componente `Header`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import ReadAll from "./components/ReadAll/ReadAll";
 
 /**
@@ -27,6 +28,7 @@ import ReadAll from "./components/ReadAll/ReadAll";
 function App() {
   return (
     <div className="App">
+      <Header />
       <ReadAll />
     </div>
   );
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <img src="https://oceanbrasil.com/assets/logo.svg" alt="Logo do Ocean" />
+      <a href="#">Home</a>
+      <a href="#">Criar</a>
+    </div>
+  );
+}
+
+export default Header;
